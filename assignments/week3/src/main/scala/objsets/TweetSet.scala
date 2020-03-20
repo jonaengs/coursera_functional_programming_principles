b2477b680d3c22ef2eefa37df42a80ca43712ae0
Jonatan <jonaengs2gmail.com>
2020-03-20 21:32:39 +0100

    complete assignment 3

--- assignments/week3/src/main/scala/objsets/TweetSet.scala
+++ assignments/week3/src/main/scala/objsets/TweetSet.scala
@@ -2,6 +2,8 @@ package objsets
 
 import TweetReader._
 
+import scala.annotation.tailrec
+
 /**
  * A class to represent tweets.
  */
@@ -34,38 +36,13 @@ class Tweet(val user: String, val text: String, val retweets: Int) {
  */
 abstract class TweetSet extends TweetSetInterface {
 
-  /**
-   * This method takes a predicate and returns a subset of all the elements
-   * in the original set for which the predicate is true.
-   *
-   * Question: Can we implment this method here, or should it remain abstract
-   * and be implemented in the subclasses?
-   */
-  def filter(p: Tweet => Boolean): TweetSet = ???
+  def isEmpty: Boolean
 
-  /**
-   * This is a helper method for `filter` that propagetes the accumulated tweets.
-   */
-  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet
+  def filter(p: Tweet => Boolean): TweetSet
 
-  /**
-   * Returns a new `TweetSet` that is the union of `TweetSet`s `this` and `that`.
-   *
-   * Question: Should we implment this method here, or should it remain abstract
-   * and be implemented in the subclasses?
-   */
-  def union(that: TweetSet): TweetSet = ???
+  def union(that: TweetSet): TweetSet
 
-  /**
-   * Returns the tweet from this set which has the greatest retweet count.
-   *
-   * Calling `mostRetweeted` on an empty set should throw an exception of
-   * type `java.util.NoSuchElementException`.
-   *
-   * Question: Should we implment this method here, or should it remain abstract
-   * and be implemented in the subclasses?
-   */
-  def mostRetweeted: Tweet = ???
+  def mostRetweeted: Tweet
 
   /**
    * Returns a list containing all tweets of this set, sorted by retweet count
@@ -73,10 +50,8 @@ abstract class TweetSet extends TweetSetInterface {
    * have the highest retweet count.
    *
    * Hint: the method `remove` on TweetSet will be very useful.
-   * Question: Should we implment this method here, or should it remain abstract
-   * and be implemented in the subclasses?
    */
-  def descendingByRetweet: TweetList = ???
+  def descendingByRetweet: TweetList
 
   /**
    * The following methods are already implemented
@@ -107,7 +82,16 @@ abstract class TweetSet extends TweetSetInterface {
 }
 
 class Empty extends TweetSet {
-  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+
+  override def isEmpty: Boolean = true
+
+  override def filter(p: Tweet => Boolean): TweetSet = this
+
+  override def union(that: TweetSet): TweetSet = that
+
+  def mostRetweeted: Tweet = throw new NoSuchElementException
+
+  def descendingByRetweet: TweetList = Nil
 
   /**
    * The following methods are already implemented
@@ -120,12 +104,43 @@ class Empty extends TweetSet {
   def remove(tweet: Tweet): TweetSet = this
 
   def foreach(f: Tweet => Unit): Unit = ()
+
 }
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+  override def isEmpty: Boolean = false
 
+  override def descendingByRetweet: TweetList = {
+    if (left.isEmpty && right.isEmpty)
+      new Cons(elem, Nil)
+    else {
+      val top = mostRetweeted
+      new Cons(top, remove(top).descendingByRetweet)
+    }
+  }
+
+  def getMax(tweets: Tweet*): Tweet = {
+    tweets.toList.maxBy(t => t.retweets)
+  }
+
+  def mostRetweeted: Tweet = {
+    if (left.isEmpty && right.isEmpty) elem
+    else if (left.isEmpty) getMax(right.mostRetweeted, elem)
+    else if (right.isEmpty) getMax(left.mostRetweeted, elem)
+    else getMax(left.mostRetweeted, right.mostRetweeted, elem)
+  }
+
+  override def union(that: TweetSet): TweetSet = {
+    if (that.isEmpty) this
+    else (left union (that incl elem)) union right
+  }
+
+  override def filter(p: Tweet => Boolean): TweetSet = {
+    val filteredChildren = left.filter(p) union right.filter(p)
+    if (p(elem)) filteredChildren incl elem
+    else filteredChildren
+  }
 
   /**
    * The following methods are already implemented
@@ -152,11 +167,13 @@ class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
     left.foreach(f)
     right.foreach(f)
   }
+
 }
 
 trait TweetList {
   def head: Tweet
   def tail: TweetList
+  def last: Tweet = if (tail.isEmpty) head else tail.last
   def isEmpty: Boolean
   def foreach(f: Tweet => Unit): Unit =
     if (!isEmpty) {
@@ -180,17 +197,18 @@ object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-  lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
+
+  lazy val googleTweets: TweetSet = TweetReader.allTweets.filter(tweet => google.exists(tweet.text.contains))
+  lazy val appleTweets: TweetSet = TweetReader.allTweets.filter(tweet => apple.exists(tweet.text.contains))
 
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
    * sorted by the number of retweets.
    */
-  lazy val trending: TweetList = ???
+  lazy val trending: TweetList = (googleTweets union appleTweets).descendingByRetweet
 }
 
 object Main extends App {
   // Print the trending tweets
-  GoogleVsApple.trending foreach println
+  GoogleVsApple.trending foreach (t => println(t.retweets))
 }
--- assignments/week3/src/test/scala/objsets/TweetSetSuite.scala
+++ assignments/week3/src/test/scala/objsets/TweetSetSuite.scala
@@ -10,9 +10,12 @@ class TweetSetSuite {
     val set3 = set2.incl(new Tweet("b", "b body", 20))
     val c = new Tweet("c", "c body", 7)
     val d = new Tweet("d", "d body", 9)
+    val e = new Tweet("e", "e body", 10)
+    val f = new Tweet("f", "f body", 31)
     val set4c = set3.incl(c)
     val set4d = set3.incl(d)
     val set5 = set4c.incl(d)
+    val set6 = (set1 incl e) incl f
   }
 
   def asSet(tweets: TweetSet): Set[Tweet] = {
@@ -43,6 +46,12 @@ class TweetSetSuite {
       assertEquals(4, size(set4c.union(set4d)))
     }
 
+  // MINE
+  @Test def `union: set5 and set6`: Unit =
+    new TestSets {
+      assertEquals(6, size(set5 union set6))
+    }
+
   @Test def `union: with empty set1`: Unit =
     new TestSets {
       assertEquals(4, size(set5.union(set1)))
@@ -55,9 +64,13 @@ class TweetSetSuite {
 
   @Test def `descending: set5`: Unit =
     new TestSets {
-      val trends = set5.descendingByRetweet
+      val set7: TweetSet = set5 union set6
+      assert(set7.mostRetweeted.user == "f")
+
+      val trends: TweetList = set7.descendingByRetweet
       assert(!trends.isEmpty)
-      assert(trends.head.user == "a" || trends.head.user == "b")
+      // assert(trends.head.user == "a" || trends.head.user == "b")
+      assert(trends.head.user == "f")
     }
 
 
